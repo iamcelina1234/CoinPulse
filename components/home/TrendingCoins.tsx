b59e9fa2101a8f48a6b6884369dfa6d8589647b3
iamcelina1234 <celinaprajapati08@gmail.com>
2026-01-08 18:52:32 +0530

    implement categories and all coins tables

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -1,5 +1,5 @@
 import { fetcher } from '@/lib/coingecko.actions';
-import { cn } from '@/lib/utils';
+import { cn, formatPercentage } from '@/lib/utils';
 import { TrendingDown, TrendingUp } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -35,12 +35,13 @@ const TrendingCoins = async () => {
       return (
         <div className={cn('price-change',isTrendingUp ? 'text-green-500':'text-red-500')}>
           <p>
+            {formatPercentage(item.data.price_change_percentage_24h.usd)}
            {isTrendingUp ? (
             <TrendingUp width={16} height={16}/>
            ):
            <TrendingDown width={16} height={16} />
           }
-          {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
+          
           </p>
         </div>
       )
--- type.d.ts
+++ type.d.ts
@@ -13,8 +13,7 @@ interface CandlestickChartProps {
   children?: React.ReactNode;
   mode?: 'historical' | 'live';
   initialPeriod?: Period;
-  liveInterval: '1s' | '1m';
-  setLiveInterval: (interval: '1s' | '1m') => void;
+  
 }
 
 interface ConverterProps {
